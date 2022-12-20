46b3cda5517a779339f42bb287551b2a55941400
mohamed-8-9-2004 <115897434+mohamed-8-9-2004@users.noreply.github.com>
2022-12-20 16:51:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1__Prime_Number_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, i, j;
+            Console.WriteLine("Enter The Range From n1 To n2");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+
+            for (i = n1; i < n2; i++)
+            {
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        break;
+
+                    }
+
+                }
+                if (i == j)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
